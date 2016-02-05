678e9bdd3cb1e8526235a7b20b289988640a96f0
Ripta Pasay <ripta@users.noreply.github.com>
2016-02-05 13:18:44 -0800

    elixir/sublist: Y U NO COMPARE?!
    Signed-off-by: Ripta Pasay <ripta@users.noreply.github.com>

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -6,7 +6,17 @@ defmodule Sublist do
   def compare([], []), do: :equal
   def compare([], _), do: :sublist
   def compare(_, []), do: :superlist
-  def compare(a, b) do
 
+  def compare(a, a), do: :equal
+
+  def compare(a, b) when length(a) < length(b) do
+    case compare(b, a) do
+      :sublist   -> :superlist
+      :superlist -> :sublist
+      other      -> other
+    end
+  end
+
+  def compare([h|t] = a, b) do
   end
 end
