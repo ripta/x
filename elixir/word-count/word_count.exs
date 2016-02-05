5c57e957cf51eee91a51500c49197782c70a758f
Ripta Pasay <ripta@users.noreply.github.com>
2016-02-04 23:51:24 -0800

    elixir/word-count: 3/9 passing
    Signed-off-by: Ripta Pasay <ripta@users.noreply.github.com>

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,4 +1,9 @@
 defmodule Words do
+  @spec count([String.t]) :: map()
+  def count(words) when is_list(words) do
+    words |> Enum.reduce(%{}, &count_helper/2)
+  end
+
   @doc """
   Count the number of words in the sentence.
 
@@ -6,6 +11,12 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
+    sentence |> String.split |> count
+  end
 
+  @spec count_helper(String.t, map()) :: map()
+  def count_helper(word, acc) do
+    # Map.update(dict, key, initial, fun) :: new_dict
+    Map.update(acc, String.downcase(word), 1, &(&1 + 1))
   end
 end
